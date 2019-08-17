3eca6b53a483d06c92ffff82a8ff8874ae50bd8f
Jon Graf <jongraf1@gmail.com>
2019-08-17 17:44:18 -0500

    not using variables for now to start up instance as causing issues

--- startContainer.sh
+++ startContainer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SQL_DOCKER_NAME=Db2Test
-CONTAINER_NAME=db2-server:latest
+CONTAINER_NAME=db2-server
 INSTANCE_NAME=db2inst1
 SQL_PASSWORD=password
 SQL_NAME=FAKEDATABASE
@@ -10,14 +10,13 @@ SQL_PORT=50000
 function startContainer {
     echo "----------Starting Up Docker Container----------"
     docker run --privileged=true --name=${SQL_DOCKER_NAME} -d=true -p "$SQL_PORT:$SQL_PORT" \
-    -e LICENSE=accept \
-    -e DB2INSTANCE=${INSTANCE_NAME} \
-    -e DB2INST1_PASSWORD=${SQL_PASSWORD} \
-    -e DBNAME=${SQL_NAME} \
-    -e ARCHIVE_LOGS=false \
-    -e AUTOCONFIG=false \
+    -e 'LICENSE=accept' \
+    -e 'DB2INSTANCE=db2inst1' \
+    -e 'DB2INST1_PASSWORD=password' \
+    -e 'DBNAME=FAKEDB' \
+    -e 'ARCHIVE_LOGS=false' \
+    -e 'AUTOCONFIG=false' \
     ${CONTAINER_NAME}
 }
 
 startContainer
-
