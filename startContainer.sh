22c28f9a4bde8f837f286e3f55844a329575ab6e
Jon Graf <jongraf1@gmail.com>
2019-08-17 17:46:54 -0500

    added check to make sure container hasnt exited before waiting

--- startContainer.sh
+++ startContainer.sh
@@ -6,10 +6,17 @@ INSTANCE_NAME=db2inst1
 SQL_PASSWORD=password
 SQL_NAME=FAKEDATABASE
 SQL_PORT=50000
+
+RED='\033[0;31m'
 GREEN='\033[0;32m'
 WHITE='\033[0m'
 
 function waitForHealthyContainer {
+    if [[ "$(docker inspect -f='{{.State.Status}}' ${SQL_DOCKER_NAME})" = "exited" ]]
+    then
+        echo -e "${RED}ERROR: Docker container exited prematurely!!!"
+        exit 1
+    fi
     until [[ "$(docker inspect -f='{{.State.Health.Status}}' ${SQL_DOCKER_NAME})" = "healthy" ]]; do
         echo "...waiting for container to become healthy..."
         sleep 1
