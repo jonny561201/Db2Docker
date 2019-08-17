e69da95ed1f6404849d0920145aa99b91082c7a2
Jon Graf <jongraf1@gmail.com>
2019-08-17 18:00:55 -0500

    fixed use of variables and added basis for flyway

--- startContainer.sh
+++ startContainer.sh
@@ -2,9 +2,9 @@
 
 SQL_DOCKER_NAME=Db2Test
 CONTAINER_NAME=db2-server
-INSTANCE_NAME=db2inst1
+INSTANCE_NAME="db2inst1"
 SQL_PASSWORD=password
-SQL_NAME=FAKEDATABASE
+SQL_NAME=FAKEDB
 SQL_PORT=50000
 
 RED='\033[0;31m'
@@ -24,13 +24,15 @@ function waitForHealthyContainer {
     echo -e "${GREEN}Container is healthy!${WHITE}"
 }
 
+
+
 function startContainer {
     echo "----------Starting Up Docker Container----------"
     docker run --privileged=true --name=${SQL_DOCKER_NAME} -d=true -p "$SQL_PORT:$SQL_PORT" \
     -e 'LICENSE=accept' \
-    -e 'DB2INSTANCE=db2inst1' \
-    -e 'DB2INST1_PASSWORD=password' \
-    -e 'DBNAME=FAKEDB' \
+    -e DB2INSTANCE=${INSTANCE_NAME} \
+    -e DB2INST1_PASSWORD=$SQL_PASSWORD \
+    -e DBNAME=${SQL_NAME} \
     -e 'ARCHIVE_LOGS=false' \
     -e 'AUTOCONFIG=false' \
     ${CONTAINER_NAME}
