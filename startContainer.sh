082097f3ff16b8d7d470790dd2a81952c92a0f8d
Jon Graf <jongraf1@gmail.com>
2019-08-17 17:46:01 -0500

    added wait to ensure docker container is healthy

--- startContainer.sh
+++ startContainer.sh
@@ -6,6 +6,16 @@ INSTANCE_NAME=db2inst1
 SQL_PASSWORD=password
 SQL_NAME=FAKEDATABASE
 SQL_PORT=50000
+GREEN='\033[0;32m'
+WHITE='\033[0m'
+
+function waitForHealthyContainer {
+    until [[ "$(docker inspect -f='{{.State.Health.Status}}' ${SQL_DOCKER_NAME})" = "healthy" ]]; do
+        echo "...waiting for container to become healthy..."
+        sleep 1
+    done
+    echo -e "${GREEN}Container is healthy!${WHITE}"
+}
 
 function startContainer {
     echo "----------Starting Up Docker Container----------"
@@ -20,3 +30,4 @@ function startContainer {
 }
 
 startContainer
+waitForHealthyContainer
