9e627cbe452c21f34098600e4fec313fe24258f3
Jon Graf <jongraf1@gmail.com>
2019-08-17 16:45:45 -0500

    created db2 docker file and script to create image

---
+++ createImage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CONTAINER_NAME=db2-server:latest
+
+function createImage {
+    echo "----------Creating Image----------"
+    docker build -t $CONTAINER_NAME $CURRENT_DIR/docker/db2Sql
+}
